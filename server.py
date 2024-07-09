6d93753b3b5166d8b7a7076b52e6e187bc4dd777
Aditya Gupta <75029064+aforaditya@users.noreply.github.com>
2024-07-09 10:35:08 +0000

    Update

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     session_data = request.cookies.get('session')
-    user_data = pickle.loads(base64.b64decode(session_data))
+    user_data = session_data and pickle.loads(base64.b64decode(session_data))
     return 'Hello from web server'
 
 app.run(debug=True)
